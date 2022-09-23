7ee12e015ef0b3036175a22a38de39e365daf7da
BuildTools <unconfigured@null.spigotmc.org>
2022-09-23 13:34:44 +0100

    presale test

--- backend/src/config.js
+++ backend/src/config.js
@@ -62,10 +62,10 @@ const MINT_PRICE = 0.00420; // Minting price per NFT. Rinkeby = ETH, Ethereum = 
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-09-23T09:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-09-23T15:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-09-23T09:15:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-09-23T15:00:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 750; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xa844ABd49bb2E68Cc2Fd4f1B28A8bfd5a5Ec2D1D"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
